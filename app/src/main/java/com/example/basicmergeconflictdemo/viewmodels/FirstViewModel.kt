5fb3a3e48850379fd44c1ebb309de3009411ca1d
rebootd <joshc@computerist.co>
2022-05-11 13:47:26 -0400

    conflict change in change2

--- app/src/main/java/com/example/basicmergeconflictdemo/viewmodels/FirstViewModel.kt
+++ app/src/main/java/com/example/basicmergeconflictdemo/viewmodels/FirstViewModel.kt
@@ -3,5 +3,6 @@ package com.example.basicmergeconflictdemo.viewmodels
 import androidx.lifecycle.ViewModel
 
 class FirstViewModel  : ViewModel() {
-
+    val name = "stupid name"
+    var isVisible = true
 }
